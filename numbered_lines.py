e778674ba47fc92c83d3dc81b218c3dc4063d450
Jolly <jollylst@hotmail.com>
2016-08-17 10:25:04 -0700

    one more file

--- numbered_lines.py
+++ numbered_lines.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+def numbered_lines():
+    with open('small.txt', 'r') as f_1:
+        lines = (f_1.readlines())
+        with open('small_2.txt', 'w') as f_2:
+            num = 0
+            for line in lines:
+                num += 1
+                f_2.write(str(num) + ' ' + line)
+
+
 
 
 def main():
